7785660c2c60ebed995097b66cd14e8208e7be54
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2022-04-22 15:16:44 +0300

    Resize table is done

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,47 @@
+import {$} from '../../assets/core/dom'
+
+export function TableResizeHendler($root, event){
+    const $resizer = $(event.target)
+    const $parent = $resizer.closest('[data-type="resizeble"]')
+    const coords = $parent.getCoords()
+    const type = $resizer.data.resize
+    const siteProperty = type === 'column' ? 'bottom' : 'right'
+    let value
+
+    $resizer.css({
+        opacity: 1,
+        zIndex: 1000,
+        [siteProperty]: '-5000px'
+    })
+
+    const cells = $root.findAll(`[data-column="${$parent.data.column}"]`)
+
+    document.onmousemove = e => {
+        if (type === 'column') {
+             const delta = e.pageX - coords.right
+            value = coords.width + delta
+            $resizer.css({right: -delta + 'px'})
+        } else {
+            const delta = e.pageY - coords.bottom
+            value = coords.height + delta
+            $resizer.css({bottom: -delta + 'px'})
+        }
+    }
+    document.onmouseup = () => {
+        document.onmousemove = null
+        document.onmouseup = null
+        if (type === 'column') {
+            $parent.css({width: value + 'px'})
+            cells.forEach(el => el.style.width = value + 'px')
+        } else {
+            $parent.css({height: value + 'px'})
+        }
+
+        $resizer.css({
+            opacity: 0,
+            bottom: 0,
+            right: 0
+        })
+    }
+}
+
