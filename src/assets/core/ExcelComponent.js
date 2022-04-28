88f7cf7528459730f6c43a71f4874dd48d812c6b
EremeevAlexey <Eremeev.Alexey.Sergeevich@gmail.com>
2022-04-28 15:09:48 +0300

    Logic is done

--- src/assets/core/ExcelComponent.js
+++ src/assets/core/ExcelComponent.js
@@ -4,11 +4,27 @@ export class ExcelComponent extends DomListener{
     constructor($root, options = {}){
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubsribes = []
+
+        this.prepare()
+    }
+
+    prepare(){
     }
 
     // Возврат шаблона компонента
     toHTML(){
         return ''
+    }
+
+    $emit(event, ...args){
+        this.emitter.emit(event, ...args)
+    }
+
+    $on(event, fn){
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubsribes.push(unsub)
     }
 
     init(){
@@ -17,5 +33,6 @@ export class ExcelComponent extends DomListener{
 
     destroy(){
         this.removeDOMListeners()
+        this.unsubsribes.forEach(unsub => unsub())
     }
 }
--- src/assets/core/utils.js
+++ src/assets/core/utils.js
@@ -5,3 +5,42 @@ export function capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+export function range(start, end){
+    if (start > end){
+        [end, start] = [start, end]
+    }
+    return new Array(end - start + 1)
+        .fill('')
+        .map((_, index) =>start + index)
+}
+
+export function matrix(target, current){
+    const columns = range(current.column, target.column)
+    const rows = range(current.row, target.row)
+
+    return columns.reduce((acc, column) => {
+        rows.forEach( row => acc.push(`${row}:${column}`))
+        return acc
+    }, [])
+}
+
+export function nextSelector(key, {column, row}){
+    const minValue = 0
+    switch (key){
+        case 'Enter':
+        case 'ArrowDown':
+            row++
+            break
+        case 'ArrowRight':
+        case 'Tab':
+            column++
+            break
+        case 'ArrowLeft':
+            column = column - 1 < minValue ? minValue : column - 1;
+            break
+        case 'ArrowUp':
+            row = row - 1 < minValue ? minValue : row - 1;
+            break
+    }
+    return `[data-id="${row}:${column}"]`
+}
